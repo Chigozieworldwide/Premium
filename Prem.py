d5ead261aaa2ebc764ba35548ee30a5d783b2f16
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-02 10:42:00 +0100

    Prem.py

--- Prem.py
+++ Prem.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ZETHON").main()
+		__import__("ZETHON").keyx()
 	else:
-		__import__("ZETHON").main()
+		__import__("ZETHON").keyx()
